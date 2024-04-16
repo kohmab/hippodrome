586e653615966f4efa157109b314d5d463c6d7cc
kohmab <kohmab@gmail.com>
2024-04-16 15:45:29 +0300

    add logging

--- src/main/java/Hippodrome.java
+++ src/main/java/Hippodrome.java
@@ -1,3 +1,6 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -8,14 +11,18 @@ public class Hippodrome {
 
     private final List<Horse> horses;
 
+    private static final Logger logger = LoggerFactory.getLogger(Hippodrome.class);
     public Hippodrome(List<Horse> horses) {
         if (isNull(horses)) {
+            logger.error("Horses list is null");
             throw new IllegalArgumentException("Horses cannot be null.");
         } else if (horses.isEmpty()) {
+            logger.error("Horses list is empty");
             throw new IllegalArgumentException("Horses cannot be empty.");
         }
-
+        logger.debug("Создание Hippodrome, лошадей [{}]",horses.size());
         this.horses = horses;
+
     }
 
     public List<Horse> getHorses() {
